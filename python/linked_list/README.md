ce12446db88f3f819a37449adc2f68476a4f9806
Tasneemalabsi <tasneem.alabsi@gmail.com>
2021-10-15 04:09:14 +0300

    edit readme in the linked list folder

---
+++ python/linked_list/README.md
@@ -0,0 +1,14 @@
+# Singly Linked List
+singly linked list describes a linked list that only moves in one direction
+
+## Challenge
+create a singly linked list with inserting a new node into it, check existing values, and a string that represents all the values in the linked list .
+
+## Approach & Efficiency
+- create a class for the linked list
+- create a constructor function to implement data in the first node
+- create function for testing and inserting new data
+
+
+## API
+<!-- Description of each method publicly available to your Linked List -->
--- python/linked_list/linked_list.py
+++ python/linked_list/linked_list.py
@@ -1,12 +1,48 @@
+class Node:
+  """
+  A class representing a Node
+
+  Attributes
+  ----------
+
+
+  Methods
+  -------
+  __init__(data, next_):
+      the constructor method for the class, it takes two parameters, the data parameter is the a reference to the data the node will hold, and the next_
+
+  """
+
+  def __init__(self, data, next_ = None):
+    self.data = data
+    self.next = next_
+
 class LinkedList:
-    """
-    Put docstring here
-    """
+  """
+  A class for creating instances of a Linked List.
 
-    def __init__(self):
-        # initialization here
-        pass
+  Data and other attributes defined here:
 
-    def some_method(self):
-        # method body here
-        pass
+  head: Node | None
+
+  Methods defined here
+
+  insert(value: any)
+  contains(value: any) -> bool
+  """
+
+  def __init__(self):
+    self.head = None
+
+  def insert(self, value):
+    """"
+    Insert creates a Node with the value that was passed and adds
+    it to the head of the linked list shifting all other values down
+
+    arguments:
+    value : any
+
+    returns: None
+    """
+    # create new node
+    self.head = Node(value, self.head)
